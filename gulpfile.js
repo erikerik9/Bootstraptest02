d4ccee52b5ce87712e7ed5cfeb42ef91608929c0
zd3H03 <zderinco@gmail.com>
2019-12-25 08:55:46 +0900

    add images

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
-{
-  "name": "test",
-  "private": true,
-  "devDependencies": {
-    "browser-sync": "^2.26.7",
-    "del": "^5.1.0",
-    "gulp": "^3.9.1",
-    "gulp-plumber": "^1.2.1",
-    "gulp-sass": "^4.0.2"
-  }
-}
+'use strict';
+
+let gulp = require('gulp');
+let browserSync = require('browser-sync').create();
+let sass = require('gulp-sass');
+let del = require('del');
+let plumber = require('gulp-plumber');
+
+gulp.task('delHtml',function(){
+  del(['html/*.html']);
+})
+gulp.task('delSass',function(){
+  del(['html/css/*.css']);
+})
+gulp.task("copy",function(){
+    return gulp.src(['assets/**/*.html'])
+    .pipe(plumber())
+    .pipe(gulp.dest('html/'))
+    .pipe(browserSync.stream());
+});
+gulp.task('sass',function(){
+  return gulp.src(['assets/sass/**/*.scss'])
+  .pipe(plumber())
+  .pipe(sass({outputStyle:'expanded'}))
+  .pipe(gulp.dest('html/css/'))
+  .pipe(browserSync.stream());
+})
+gulp.task('default',['delHtml','delSass','copy','sass'],function(){
+  browserSync.init({
+    server: {
+      baseDir:"html"
+    },
+    browser: 'chrome'
+  });
+    gulp.watch(['assets/**/*.html'],['delHtml','copy']);
+    gulp.watch(['assets/sass/*.scss'],['delSass','sass']);
+});
